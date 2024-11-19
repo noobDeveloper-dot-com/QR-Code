4a12b784648a491c2979f6dcf49967af92160c1b
Abdul <abdul@365.lk>
2024-11-19 07:01:25 +0530

    Refactored QR Code

--- src/API_Call.js
+++ src/API_Call.js
@@ -1,72 +1,74 @@
-import { useState, useEffect } from "react"
-import { QrReader } from "react-qr-reader"
+import { useState, useEffect } from "react";
+import { QrReader } from "react-qr-reader";
 
+function Api_Call() {
+  const [referenceNum, setReferenceNum] = useState("");
+  const [selectedData, setSelectedData] = useState(null);
+  const [data, setData] = useState("No Result");
 
+  useEffect(() => {
+    console.log(selectedData);
+  }, [selectedData]);
 
-function Api_Call(){
-    const[referenceNum, setReferenceNum] = useState("")
-    const[selectedData, setSelectedData] = useState()
-    const[data,setData] = useState('No Result') 
-
-    useEffect(()=>{
-     console.log(selectedData)
-    },[selectedData])
-
-    function fetchData(){
-        console.log(referenceNum)
-        if(!referenceNum){
-           alert('Please enter a reference number')
-            return
-        }
- fetch(`https://673adcd2339a4ce445196d93.mockapi.io/api/v1/users`)
-    .then((resp)=>{
-        return resp.json()
-
-    })
-    .then((parseData)=>{
-      const selectedDataObj = parseData.find(({referenceNo})=>{
-        return referenceNo === referenceNum
+  function fetchData() {
+    if (!referenceNum) {
+      alert("Please enter a reference number");
+      return;
     }
-    
-   
-)
-        
-        if(!selectedDataObj){
-            setSelectedData(null)
-            return
+
+    fetch("https://673adcd2339a4ce445196d93.mockapi.io/api/v1/users")
+      .then((resp) => resp.json())
+      .then((parseData) => {
+        const selectedDataObj = parseData.find(
+          ({ referenceNo }) => referenceNo === referenceNum
+        );
+
+        if (!selectedDataObj) {
+          setSelectedData(null);
+          return;
         }
-       setSelectedData(selectedDataObj)
-})}
+        setSelectedData(selectedDataObj);
+      });
+  }
 
-
-    return (
-        <>
-         <div className="ref-wrapper flexB">
-          <input className="referenceNo" value={referenceNum} type="text" onChange={(e)=>{setReferenceNum(e.target.value)}}/>
-          <button className="api_call_btn" onClick={fetchData}>Submit</button>
-         </div>
-         <div className="displayData">
-            {selectedData? <div>
-                <p>Product name: {selectedData.productName}</p>
-                <br/>
-                <p>Product info: {selectedData.description}</p>
-            </div>: <p>Data Not Found</p>}
-         </div>
-         <QrReader
-           onResult={(result, error) =>{
-            if(!!result){
-                setData(result?.text)
-            }
-
-            if(!!error){
-                console.info(error)
-            }
-           }}
-           style={{width: '100%'}}
-         />
-         <p>{data}</p>
-        </>
-        
-    )
+  return (
+    <>
+      <div className="ref-wrapper flexB">
+        <input
+          className="referenceNo"
+          value={referenceNum}
+          type="text"
+          onChange={(e) => setReferenceNum(e.target.value)}
+        />
+        <button className="api_call_btn" onClick={fetchData}>
+          Submit
+        </button>
+      </div>
+      <div className="displayData">
+        {selectedData ? (
+          <div>
+            <p>Product name: {selectedData.productName}</p>
+            <br />
+            <p>Product info: {selectedData.description}</p>
+          </div>
+        ) : (
+          <p>Data Not Found</p>
+        )}
+      </div>
+      <QrReader
+        onResult={(result, error) => {
+          if (result) {
+            setReferenceNum(result.text);
+            setData(result.text);
+          } else if (error) {
+            console.error(error);
+          }
+        }}
+        style={{ width: "100%" }}
+      />
+      <p>{data}</p>
+    </>
+  );
 }
-export default Api_Call
+
+export default Api_Call;
